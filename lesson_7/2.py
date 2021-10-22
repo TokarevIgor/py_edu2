ff91484213f2d433914603227012dd3fd60dcb10
tokarev_igor <tokarev.igor93@gmail.com>
2021-10-22 15:20:13 +0600

    Tokarev Igor home_work 7

---
+++ lesson_7/2.py
@@ -0,0 +1,58 @@
+# 2. Отсортируйте по возрастанию методом слияния одномерный вещественный массив,
+# заданный случайными числами на промежутке [0; 50). Выведите на экран исходный и отсортированный массивы.
+
+import random
+
+
+def merge(arr, l, m, r):
+    n1 = m - l + 1
+    n2 = r - m
+
+    L = [0] * (n1)
+    R = [0] * (n2)
+
+    for i in range(0, n1):
+        L[i] = arr[l + i]
+
+    for j in range(0, n2):
+        R[j] = arr[m + 1 + j]
+
+    i = 0
+    j = 0
+    k = l
+
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+
+
+def merge_sort(arr, l, r):
+    if l < r:
+        m = l + (r - l) // 2
+        merge_sort(arr, l, m)
+        merge_sort(arr, m + 1, r)
+        merge(arr, l, m, r)
+
+
+ran_ls = [random.randint(0, 50) for i in range(50)]
+print('Исходный:\n', ran_ls)
+n = len(ran_ls)
+
+merge_sort(ran_ls, 0, n - 1)
+print('Результат\n', ran_ls)
+
